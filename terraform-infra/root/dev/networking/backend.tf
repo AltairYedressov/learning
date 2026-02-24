67dab2473ed3da5f1c401dc93cb4dc298475a34e
Altoha <altair.yed@edu.312school.com>
2026-02-24 01:43:51 -0500

    fix: modified name on dynamodb table in backend

--- terraform-infra/root/dev/networking/backend.tf
+++ terraform-infra/root/dev/networking/backend.tf
@@ -6,6 +6,6 @@ terraform {
     key            = "${path_relative_to_include()}/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock-dev" # optional, for state locking
+    dynamodb_table = "372517046622-terraform-lock-dev" # optional, for state locking
   }
 }
