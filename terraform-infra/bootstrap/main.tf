28a8f4afe9fddeded5d6d4cdb522bb3750789b6f
Altoha <altair.yed@edu.312school.com>
2026-02-24 01:43:29 -0500

    fix: modified name on dynamodb table in backend

--- terraform-infra/bootstrap/main.tf
+++ terraform-infra/bootstrap/main.tf
@@ -28,3 +28,19 @@ resource "aws_s3_bucket_public_access_block" "block_public" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "${var.ACCOUNT_ID}-terraform-lock-dev"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "${var.ACCOUNT_ID}-terraform-lock-dev"
+    Environment = "dev"
+  }
+}
