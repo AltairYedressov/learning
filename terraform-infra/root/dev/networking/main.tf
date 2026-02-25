4168d9ca5d5a3028fa8a9292913384946e7eb6e6
Altoha <altair.yed@edu.312school.com>
2026-02-25 18:47:29 -0500

    chore:add tags

--- terraform-infra/root/dev/networking/main.tf
+++ terraform-infra/root/dev/networking/main.tf
@@ -13,16 +13,16 @@ module "subnets" {
 }
 
 module "igw" {
-  source = "../../../networking/igw"
-  vpc_id = module.vpc.vpc_id
+  source      = "../../../networking/igw"
+  vpc_id      = module.vpc.vpc_id
   environment = var.environment
 }
 
 module "route-tables" {
-  source     = "../../../networking/route-tables"
-  vpc_id     = module.vpc.vpc_id
-  igw_id     = module.igw.igw_id
-  subnets    = var.subnets
-  subnet_ids = module.subnets.subnet_ids
+  source      = "../../../networking/route-tables"
+  vpc_id      = module.vpc.vpc_id
+  igw_id      = module.igw.igw_id
+  subnets     = var.subnets
+  subnet_ids  = module.subnets.subnet_ids
   environment = var.environment
 }
