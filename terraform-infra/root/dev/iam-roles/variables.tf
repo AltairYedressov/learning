ca606b6ce28566d54e55632ab000af807541c4c4
Altoha <altair.yed@edu.312school.com>
2026-02-25 20:16:30 -0500

    chore: terraform format

--- terraform-infra/root/dev/iam-roles/variables.tf
+++ terraform-infra/root/dev/iam-roles/variables.tf
@@ -13,11 +13,11 @@ variable "environment" {
 }
 
 variable "eks_worker_nodes_role" {
-    type = string
-    default = "eks_worker_nodes_role"
+  type    = string
+  default = "eks_worker_nodes_role"
 }
 
 variable "eks_worker_nodes_policy" {
-    type = list(string)
-    default = [ "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess", "arn:aws:iam::aws:policy/AmazonEC2FullAccess"]
+  type    = list(string)
+  default = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess", "arn:aws:iam::aws:policy/AmazonEC2FullAccess"]
 }
