1623dfbe7ae910277f42f867ca00b38ce8df0ed8
Altoha <altair.yed@edu.312school.com>
2026-02-25 23:21:41 -0500

    fix: modify name of sg

--- terraform-infra/networking/security-group/security-group.tf
+++ terraform-infra/networking/security-group/security-group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = var.vpc_id
 
   tags = {
-    Name = "sg-${var.resource_name}-${var.environment}"
+    Name = "SecurityGroup-${var.resource_name}-${var.environment}"
   }
 }
 
