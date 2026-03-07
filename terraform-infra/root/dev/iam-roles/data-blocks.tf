3fb3dac29b979e78fc8a77e37ca792467cc3f050
Altoha <altair.yed@edu.312school.com>
2026-03-07 12:36:25 -0500

    feat: ebs csi

--- terraform-infra/root/dev/iam-roles/data-blocks.tf
+++ terraform-infra/root/dev/iam-roles/data-blocks.tf
@@ -1,4 +1,4 @@
-# data "aws_iam_openid_connect_provider" "eks_oidc_provider" {
-#   # URL of the existing OIDC provider from your EKS cluster
-#   url = "https://oidc.eks.us-east-1.amazonaws.com/id/EC88FD545343CE767DE5BBF583B16C05"
-# }
+data "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  # URL of the existing OIDC provider from your EKS cluster
+  url = "https://oidc.eks.us-east-1.amazonaws.com/id/D61E52E76FBF3DDE20ED109C2F383FF4"
+}
