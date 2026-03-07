0372461ee7bf1b3e609e783522cee26a483ed03f
Altoha <altair.yed@edu.312school.com>
2026-03-07 02:46:07 -0500

    test

--- terraform-infra/eks-cluster/asg.tf
+++ terraform-infra/eks-cluster/asg.tf
@@ -1,49 +1,49 @@
-resource "aws_autoscaling_group" "workers_asg" {
-  name                = "${var.cluster_name}-workers-asg"
-  min_size            = var.min_size
-  max_size            = var.max_size
-  desired_capacity    = var.desired_capacity
-  vpc_zone_identifier = data.aws_subnets.public.ids
+# resource "aws_autoscaling_group" "workers_asg" {
+#   name                = "${var.cluster_name}-workers-asg"
+#   min_size            = var.min_size
+#   max_size            = var.max_size
+#   desired_capacity    = var.desired_capacity
+#   vpc_zone_identifier = data.aws_subnets.public.ids
 
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.workers_lt.id
-        version            = "$Latest"
-      }
+#   mixed_instances_policy {
+#     launch_template {
+#       launch_template_specification {
+#         launch_template_id = aws_launch_template.workers_lt.id
+#         version            = "$Latest"
+#       }
 
-      dynamic "override" {
-        for_each = var.ec2_types
-        content {
-          instance_type = override.value
-        }
-      }
-    }
+#       dynamic "override" {
+#         for_each = var.ec2_types
+#         content {
+#           instance_type = override.value
+#         }
+#       }
+#     }
 
-    instances_distribution {
-      on_demand_base_capacity                  = 0
-      on_demand_percentage_above_base_capacity = 20
-      spot_allocation_strategy                 = "lowest-price"
-    }
-  }
+#     instances_distribution {
+#       on_demand_base_capacity                  = 0
+#       on_demand_percentage_above_base_capacity = 20
+#       spot_allocation_strategy                 = "lowest-price"
+#     }
+#   }
 
-  tag {
-    key                 = "kubernetes.io/cluster/${var.cluster_name}"
-    value               = "owned"
-    propagate_at_launch = true
-  }
+#   tag {
+#     key                 = "kubernetes.io/cluster/${var.cluster_name}"
+#     value               = "owned"
+#     propagate_at_launch = true
+#   }
 
-  tag {
-    key                 = "Name"
-    value               = "${var.cluster_name}-worker-node"
-    propagate_at_launch = true
-  }
+#   tag {
+#     key                 = "Name"
+#     value               = "${var.cluster_name}-worker-node"
+#     propagate_at_launch = true
+#   }
 
-  tag {
-    key                 = "environment"
-    value               = var.environment
-    propagate_at_launch = true
-  }
+#   tag {
+#     key                 = "environment"
+#     value               = var.environment
+#     propagate_at_launch = true
+#   }
 
-  depends_on = [aws_launch_template.workers_lt]
-}
+#   depends_on = [aws_launch_template.workers_lt]
+# }
