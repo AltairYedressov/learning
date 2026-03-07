95ebea6acdab962de512195453f6af82a4bc7a05
Altoha <altair.yed@edu.312school.com>
2026-03-07 12:18:26 -0500

    fix: worker node iam role

--- terraform-infra/root/dev/iam-roles/main.tf
+++ terraform-infra/root/dev/iam-roles/main.tf
@@ -22,26 +22,26 @@ module "eks_worker_nodes" {
   aws_managed_policy_arns = var.eks_worker_nodes_policy
 }
 
-module "ebs_csi_irsa_role" {
-  source = "../../../iam-role-module"
+# module "ebs_csi_irsa_role" {
+#   source = "../../../iam-role-module"
 
-  role_name   = var.ebs_csi_irsa_role
-  environment = var.environment
+#   role_name   = var.ebs_csi_irsa_role
+#   environment = var.environment
 
-  principal_type = "Federated"
+#   principal_type = "Federated"
 
-  principal_identifiers = [
-    data.aws_iam_openid_connect_provider.eks_oidc_provider.arn
-  ]
+#   principal_identifiers = [
+#     data.aws_iam_openid_connect_provider.eks_oidc_provider.arn
+#   ]
 
-  assume_role_conditions = {
-    ebs_csi_controller = {
-      test     = "StringEquals"
-      variable = "${replace(data.aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-    }
-  }
-  aws_managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  ]
-}
+#   assume_role_conditions = {
+#     ebs_csi_controller = {
+#       test     = "StringEquals"
+#       variable = "${replace(data.aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#     }
+#   }
+#   aws_managed_policy_arns = [
+#     "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#   ]
+# }
