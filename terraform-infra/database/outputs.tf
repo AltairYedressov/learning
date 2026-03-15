89f84b0b85adf7f67598b8eb524d1a04f7270600
Altoha <altair.yed@edu.312school.com>
2026-03-15 15:29:17 -0400

    feat: created child module for database with DR plan

---
+++ terraform-infra/database/outputs.tf
@@ -0,0 +1,30 @@
+# terraform-infra/database/outputs.tf
+output "db_endpoint" {
+  value       = aws_db_instance.default.endpoint
+  description = "Primary database endpoint"
+}
+
+output "db_port" {
+  value       = aws_db_instance.default.port
+  description = "Database port"
+}
+
+output "db_name" {
+  value       = aws_db_instance.default.db_name
+  description = "Database name"
+}
+
+output "db_arn" {
+  value       = aws_db_instance.default.arn
+  description = "Database ARN"
+}
+
+output "dr_replica_endpoint" {
+  value       = var.create_dr_replica ? aws_db_instance.dr_replica[0].endpoint : null
+  description = "DR replica endpoint - promote this if primary region fails"
+}
+
+output "subnet_group_name" {
+  value       = aws_db_subnet_group.default.name
+  description = "DB subnet group name"
+}
