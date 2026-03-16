8b90d6a39b7574683dc36b38352164fd37a4b012
Altoha <altair.yed@edu.312school.com>
2026-03-15 21:38:29 -0400

    feat:add iam role for velero

--- terraform-infra/root/dev/iam-roles/main.tf
+++ terraform-infra/root/dev/iam-roles/main.tf
@@ -74,3 +74,29 @@ module "karpenter_irsa_role" {
   }
   custom_policy_json_path = "${path.module}/../../../iam-role-module/Policies/karpenter_policy.json"
 }
+
+module "velero_irsa_role" {
+  source = "../../../iam-role-module"
+
+  role_name          = "velero-role"
+  environment        = var.environment
+  assume_role_action = "sts:AssumeRoleWithWebIdentity"
+  principal_type     = "Federated"
+  principal_identifiers = [
+    data.aws_iam_openid_connect_provider.eks_oidc_provider.arn
+  ]
+  assume_role_conditions = {
+    sub = {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:velero:velero"]
+    }
+    aud = {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_iam_openid_connect_provider.eks_oidc_provider.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+  custom_policy_json_path = "${path.module}/../../../iam-role-module/Policies/velero_policy.json"
+  aws_managed_policy_arns = []
+}
