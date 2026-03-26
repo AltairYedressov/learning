bd17b8da470263f16061aa847a0822bc58e22f09
Altoha <altair.yed@edu.312school.com>
2026-03-26 19:39:21 -0400

    feat: create record

--- terraform-infra/root/dev/dns/main.tf
+++ terraform-infra/root/dev/dns/main.tf
@@ -5,3 +5,15 @@ module "acm" {
   zone_id                   = data.aws_route53_zone.this.zone_id
   environment               = var.environment
 }
+
+resource "aws_route53_record" "istio_ingress" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.istio_ingress_lb_hostname
+    zone_id                = "Z26RNL4JYFTOTI" # NLB hosted zone ID for us-east-1
+    evaluate_target_health = true
+  }
+}
