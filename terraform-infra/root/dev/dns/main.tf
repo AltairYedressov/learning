eeef4aa60a08b285e7b7228610fba93e839e2892
Altoha <altair.yed@edu.312school.com>
2026-03-26 00:08:03 -0400

    feat: create TLS certificate for domain

--- terraform-infra/root/dev/dns/main.tf
+++ terraform-infra/root/dev/dns/main.tf
@@ -1,5 +1,5 @@
 module "acm" {
-  source                    = "../../../networking/acm"
+  source                    = "../../../dns/acm"
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   zone_id                   = data.aws_route53_zone.this.zone_id
